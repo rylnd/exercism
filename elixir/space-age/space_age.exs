4325a23a8ceed366126c469121236c465abce1f2
Ryland Herrick <ryalnd@gmail.com>
2017-01-29 18:18:30 -0600

    Implement age_on function
    Uses a map literal to declare the mapping of planets to orbital periods
    (relative to earth).

--- elixir/space-age/space_age.exs
+++ elixir/space-age/space_age.exs
@@ -8,6 +8,17 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
+    earth_seconds = 31557600
 
+    seconds / %{
+      :earth => 1,
+      :mercury => 0.2408467,
+      :venus => 0.61519726,
+      :mars => 1.8808158,
+      :jupiter => 11.862615,
+      :saturn => 29.447498,
+      :uranus => 84.016846,
+      :neptune => 164.79132,
+    }[planet] / earth_seconds
   end
 end
