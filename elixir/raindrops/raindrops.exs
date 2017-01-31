a98968f37c9f9e00964afc6402bfc45772c15587
Ryland Herrick <ryalnd@gmail.com>
2017-01-31 00:46:05 -0600

    Implement fizzbuzz(ish) as pipe of anonymous functions
    Admittedly less readable than the empirical equivalent, but it was fun
    to make this implementation work. Can't wait to see others' answers.

--- elixir/raindrops/raindrops.exs
+++ elixir/raindrops/raindrops.exs
@@ -10,6 +10,10 @@ defmodule Raindrops do
   """
   @spec convert(pos_integer) :: String.t
   def convert(number) do
-
+    ""
+    |> (&(if rem(number, 3) == 0, do: &1 <> "Pling", else: &1)).()
+    |> (&(if rem(number, 5) == 0, do: &1 <> "Plang", else: &1)).()
+    |> (&(if rem(number, 7) == 0, do: &1 <> "Plong", else: &1)).()
+    |> (&(if String.length(&1) == 0, do: Integer.to_string(number), else: &1)).()
   end
 end
