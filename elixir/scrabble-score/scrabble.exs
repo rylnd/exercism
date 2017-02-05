e1fb2b8cfada2667f81d4a60426bbbf0d76cad34
Ryland Herrick <ryalnd@gmail.com>
2017-02-05 16:25:24 -0600

    Implement scrabble score as reduction over score function
    There's probably a better way to do the character scoring, but this
    works.

--- elixir/scrabble-score/scrabble.exs
+++ elixir/scrabble-score/scrabble.exs
@@ -4,6 +4,23 @@ defmodule Scrabble do
   """
   @spec score(String.t) :: non_neg_integer
   def score(word) do
+    word
+    |> String.downcase
+    |> String.codepoints
+    |> Enum.map(&character_score/1)
+    |> Enum.sum
+  end
 
+  defp character_score(char) do
+    cond do
+      char in ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"] -> 1
+      char in ["d", "g"] -> 2
+      char in ["b", "c", "m", "p"] -> 3
+      char in ["f", "h", "v", "w", "y"] -> 4
+      char in ["k"] -> 5
+      char in ["j", "x"] -> 8
+      char in ["q", "z"] -> 10
+      true -> 0
+    end
   end
 end
