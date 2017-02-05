79ef91eebfa2ce00cac694786ab986b4c1485e93
Ryland Herrick <ryalnd@gmail.com>
2017-02-05 15:36:40 -0600

    Implements sublist/superlist determination
    Defines a recursive function that returns whether or not the first list
    is contained in its entirety in the second list.
    
    If it is, it is a superlist. If the second list is contained entirely
    within the first, then the first is a sublist.

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -4,6 +4,18 @@ defmodule Sublist do
   and if not whether it is equal or unequal to the second list.
   """
   def compare(a, b) do
-
+    cond do
+      a === b -> :equal
+      contains(a, b) -> :superlist
+      contains(b, a) -> :sublist
+      true -> :unequal
+    end
   end
+
+  defp contains(_, []), do: true
+  defp contains([], _), do: false
+  defp contains([x|a], [x|b]) do
+    Enum.take(a, Enum.count(b)) === b || contains(a, [x|b])
+  end
+  defp contains([_|a], [y|b]), do: contains(a, [y|b])
 end
